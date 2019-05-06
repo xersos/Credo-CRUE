0b551e36fadf7fe2051c67ae77bde86da43b504b
coco <jacobcorentin@yahoo.fr>
2019-05-06 10:36:44 +0200

    ok my name is jafe

--- cep/src/app/app.module.ts
+++ cep/src/app/app.module.ts
@@ -1,26 +1,39 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
+
+import { LayoutModule } from '@angular/cdk/layout';
+import { OverlayModule } from '@angular/cdk/overlay';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from './materialmodule/material.module';
 import {FlexLayoutModule} from '@angular/flex-layout';
 
-import {AppComponent} from './app.component';
-import {LayoutComponent} from './layout/layout.component';
-import {HomeComponent} from './home/home.component';
-import {RoutingModule} from './routing/routing.module';
-import {HeaderComponent} from './navigation/header/header.component';
-import {FooterComponent} from './navigation/footer/footer.component';
-import {GridPresse} from './home/grid-presse/grid-presse.component';
-import {LoginComponent} from './login/login.component';
-import {ReactiveFormsModule} from '@angular/forms';
+import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { HomeComponent } from './home/home.component';
+import { RoutingModule } from './routing/routing.module';
+import { HeaderComponent } from './navigation/header/header.component';
+import { FooterComponent } from './navigation/footer/footer.component';
+import { GridPresse } from './home/grid-presse/grid-presse.component';
+import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import {HTTP_INTERCEPTORS, HttpClientModule, HttpClient} from '@angular/common/http';
-import {MatCardModule, MatInputModule, MatProgressSpinnerModule} from '@angular/material';
-import {LoaderComponent} from './loader/loader.component';
-import {LoaderService} from './services/loader/loader.service';
-import {LoaderInterceptor} from './interceptors/loader/loader.interceptor';
+import { HTTP_INTERCEPTORS, HttpClientModule, HttpClient } from '@angular/common/http';
+import { MatCardModule, MatInputModule, MatProgressSpinnerModule } from '@angular/material';
+import { LoaderComponent } from './loader/loader.component';
+import { LoaderService } from './services/loader/loader.service';
+import { LoaderInterceptor } from './interceptors/loader/loader.interceptor';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export const createTranslateLoader = (http: HttpClient) => {
+  return new TranslateHttpLoader(
+      http,
+      '/start-javascript/sb-admin-material/master/dist/assets/i18n/',
+      '.json'
+  );
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+};
 
 
 
@@ -48,6 +61,15 @@ import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
     MatInputModule,
     MatCardModule,
     MatProgressSpinnerModule,
+    LayoutModule,
+    OverlayModule,
+    TranslateModule.forRoot({
+      loader: {
+          provide: TranslateLoader,
+          useFactory: createTranslateLoader,
+          deps: [HttpClient]
+      }
+  })
   ],
   providers: [LoaderService,
     {provide: HTTP_INTERCEPTORS, useClass: LoaderInterceptor, multi: true}
